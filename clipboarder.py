0a5caa715c22fdbdd7e287ae58f3212ebcf841be
James Li <jamespeilunli@gmail.com>
2024-07-01 20:50:07 -0700

    add option to preserve previous clipboard

--- clipboarder.py
+++ clipboarder.py
@@ -3,7 +3,12 @@ import pyperclip
 
 pyautogui.PAUSE = 0
 
-def run(evaluate, copy=True, paste=True):
+def run(evaluate, preserve_clipboard=True, copy=True, paste=True):
+    previous_content = None
+
+    if preserve_clipboard:
+        previous_content = pyperclip.paste()
+
     if copy:
         pyautogui.hotkey("ctrl", "c")
     
@@ -13,3 +18,7 @@ def run(evaluate, copy=True, paste=True):
 
     if paste:
         pyautogui.hotkey("ctrl", "v")
+
+    if preserve_clipboard:
+        pyautogui.sleep(0.2) # i believe pyautogui.hotkey is asynchronous
+        pyperclip.copy(previous_content)
